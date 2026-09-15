da72d0c79c7207a2194fc765202e8e8f27abd0c5
MQ-bit <41608423+MQ-bit@users.noreply.github.com>
2026-09-15 11:09:10 +0900

    feat: 압그레이드 json 저장
    -UpgradeSaveData->to Json

---
+++ Assets/02.Scripts/Upgrade/UpgradeSaveData.cs
@@ -0,0 +1,14 @@
+﻿[System.Serializable]
+public class UpgradeSaveData
+{
+
+    public string[] Name;
+    public int[] Level;
+
+    public UpgradeSaveData(int count)
+    {
+        Name= new string[count];
+        Level = new int[count];
+    }
+
+}
